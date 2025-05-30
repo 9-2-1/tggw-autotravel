1467b15209cdc234307f168e284311b2c94cc624
9-2-1 <1195132772@qq.com>
2025-05-30 23:10:42 +0800

    main: log exception

--- tggw_autotravel/main.py
+++ tggw_autotravel/main.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import traceback
 
 from .controller import Controller
 
@@ -9,25 +10,29 @@ CYCLE_TIME = 0.01
 
 
 def main() -> None:
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename="main.log",
-        filemode="a",
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        encoding="utf-8",
-    )
-    log.info("Start")
-    maingame = Controller(38, 92)
-    maingame.run()
-    while maingame.is_running():
-        while True:
-            char = maingame.getch()
-            if char == "":
-                break
-            log.debug(f"char: {char!r}")
-            # handle
-            # with errorcatcher(log):
-            maingame.write(char)
-        maingame.nextframe()
-        time.sleep(CYCLE_TIME)
-    maingame.stop()
+    try:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            filename="main.log",
+            filemode="a",
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            encoding="utf-8",
+        )
+        log.info("Start")
+        maingame = Controller(38, 92)
+        maingame.run()
+        while maingame.is_running():
+            while True:
+                char = maingame.getch()
+                if char == "":
+                    break
+                log.debug(f"char: {char!r}")
+                # handle
+                # with errorcatcher(log):
+                maingame.write(char)
+            maingame.nextframe()
+            time.sleep(CYCLE_TIME)
+        maingame.stop()
+    except Exception:
+        log.error(traceback.format_exc())
+        raise
